d4cdf39f5685e07b54bdafdf8e6df284796d3046
Misaka19986 <1657512446@qq.com>
2025-02-26 15:05:29 +0800

    update: modernize

--- hvisor-tool/tools/include/virtio_net.h
+++ hvisor-tool/tools/include/virtio_net.h
@@ -28,13 +28,9 @@ typedef struct virtio_net_dev {
 NetDev *init_net_dev(uint8_t mac[]);
 
 int virtio_net_rxq_notify_handler(VirtIODevice *vdev, VirtQueue *vq);
-
 int virtio_net_txq_notify_handler(VirtIODevice *vdev, VirtQueue *vq);
 
 void virtio_net_event_handler(int fd, int epoll_type, void *param);
-
 int virtio_net_init(VirtIODevice *vdev, char *devname);
-
 void virtio_net_close(VirtIODevice *vdev);
-
 #endif //_HVISOR_VIRTIO_NET_H
